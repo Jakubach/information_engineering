cece49c2a1e62927482a48e079dba06f8efb4b20
Jakubach <jakubach@gmail.com>
2022-01-10 12:46:04 +0100

    Update person.h

--- L9/include/person.h
+++ L9/include/person.h
@@ -16,4 +16,5 @@ Person initialise_person();
 void print_person(Person &person);
 float average_weight(const std::vector<Person>& persones);
 Person tallest_person(const std::vector<Person>& persones);
+bool compare_persones_ages(const Person &p1, const Person &p2);
 void sort_by_age(std::vector<Person>& persones);
