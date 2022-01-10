f5abc53995dfef184106cf5f44e65a56b0ce8e6b
Jakubach <jakubach@gmail.com>
2022-01-10 11:59:12 +0100

    Delete pet.h

--- L9/src/pet.h
+++
@@ -1,9 +0,0 @@
-#include "pet.h"
-
-#include <iostream>
-
-void print_pet(Pet &pet) {
-   std::cout << "Name: " << pet.name << std::endl;
-   std::cout << "Age: " << pet.age << std::endl;
-   std::cout << "Weight: " << pet.weight << std::endl;
-}
