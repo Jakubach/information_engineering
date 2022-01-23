4fcbc12545c238898924d079639068f1e5e335b3
Jakubach <jakubach@gmail.com>
2022-01-23 18:44:21 +0100

    Update main.cpp

--- L10 - test 2/main.cpp
+++ L10 - test 2/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     std::vector<VehicleProduction> vehicle_productions;
-    VehicleProduction first_production = {"First","CountryA",100};
+    VehicleProduction first_production = {"First","CountryA",500};
     VehicleProduction second_production = {"Second","CountryB",200};
     VehicleProduction third_production = {"Third","CountryC",300};
     VehicleProduction fourth_production = initialise_vehicle_production();
@@ -17,6 +17,7 @@ int main(){
     print_largest_production(vehicle_productions);
     print_smallest_production(vehicle_productions );
     sort_productions(vehicle_productions );
+    print_vector_vehicle_productions(vehicle_productions);
     //remember to add also the comparation functions (predicates) for std::algorithm functions
     return 0;
 }
